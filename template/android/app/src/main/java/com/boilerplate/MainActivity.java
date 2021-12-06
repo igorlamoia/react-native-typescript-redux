51c04ff60ed2b9b53e9bbeedc17d75baabd3104d
Raul Max <raulmax319@gmail.com>
2021-12-06 19:48:52 -0300

    fix: trying renaming app name to fix template renaming issue

--- template/android/app/src/main/java/com/boilerplate/MainActivity.java
+++ template/android/app/src/main/java/com/boilerplate/MainActivity.java
@@ -1,4 +1,4 @@
-package com.boilerplate_app;
+package com.boilerplate;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "boilerplate_app";
+    return "boilerplate";
   }
 }
--- template/ios/boilerplateTests/boilerplateTests.m
+++ template/ios/boilerplateTests/boilerplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface boilerplate_appTests : XCTestCase
+@interface boilerplateTests : XCTestCase
 
 @end
 
-@implementation boilerplate_appTests
+@implementation boilerplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
